66ca335fcd9cd950b48331866281568a6ce14c8d
TeaOk210 <danilplay21006@gmail.com>
2023-03-09 19:34:45 +0200

    Chto-to

--- app/src/main/java/com/example/sqlite/DataBaseHalper.kt
+++ app/src/main/java/com/example/sqlite/DataBaseHalper.kt
@@ -3,5 +3,16 @@ package com.example.sqlite
 import android.content.Context
 import android.database.sqlite.SQLiteOpenHelper
 
-class DataBaseHalper (Context : Context) : SQLiteOpenHelper(Context, ) {
+class DataBaseHalper (Context : Context) : SQLiteOpenHelper(Context, Table_Name , _ID, Name ) {
+
+    companion object{
+        val Table_Name = "Person"
+
+        val _ID = "_id"
+        val Name = "Name"
+        val Login = "Login"
+        val Password = "Password"
+
+
+    }
 }
