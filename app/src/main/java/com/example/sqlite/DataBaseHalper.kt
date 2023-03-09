23608253f1bbc50bc18008d2f2afe8cc874b78e3
TeaOk210 <danilplay21006@gmail.com>
2023-03-09 19:28:57 +0200

    Nothing

---
+++ app/src/main/java/com/example/sqlite/DataBaseHalper.kt
@@ -0,0 +1,7 @@
+package com.example.sqlite
+
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+
+class DataBaseHalper (Context : Context) : SQLiteOpenHelper(Context, ) {
+}
