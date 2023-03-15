e8ed091e123cc4e3c21cfc4c76d2a5ef00675c2f
TeaOk210 <danilplay21006@gmail.com>
2023-03-15 20:56:00 +0200

    Ne rabotaet!

--- app/src/main/java/com/example/sqlite/MainActivity.kt
+++ app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.sqlite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleCursorAdapter
 import com.example.sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    lateinit var dbManager: DBManager
+    val String = listOf<String>( DataBaseHalper._ID, DataBaseHalper.Login, DataBaseHalper.Password)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        dbManager = DBManager(this)
+        dbManager.open()
+
 
         supportFragmentManager
             .beginTransaction()
@@ -20,6 +27,16 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.Login, fragment_login.newInstance())
             .commit()
+
+        binding.In.setOnClickListener() {
+            val Login = Intent(this, SignIn::class.java)
+            startActivity(Login)
         }
 
+            binding.Up.setOnClickListener() {
+            val Login = binding.Login.toString()
+            val Password = binding.password.toString()
+            dbManager.update(_id = Long.MIN_VALUE, Login, Password)
+            }
+    }
 }
--- app/src/main/java/com/example/sqlite/SignIn.kt
+++ app/src/main/java/com/example/sqlite/SignIn.kt
@@ -1,11 +1,30 @@
 package com.example.sqlite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sqlite.databinding.ActivitySignInBinding
 
 class SignIn : AppCompatActivity() {
+    lateinit var binding : ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.passwordLog, fragment_pass.newInstance())
+            .commit()
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.LoginLog, fragment_login.newInstance())
+            .commit()
+
+        binding.In.setOnClickListener() {
+            val Register = Intent(this, MainActivity::class.java)
+            startActivity(Register)
+        }
     }
 }
