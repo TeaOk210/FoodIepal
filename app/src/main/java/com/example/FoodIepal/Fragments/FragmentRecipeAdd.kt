92a7e591b1ef33cf34a6d08a640d32125b483346
TeaOk210 <danilplay21006@gmail.com>
2023-04-06 19:13:03 +0200

    апи капец добавление сложно много букав много цифар

---
+++ app/src/main/java/com/example/FoodIepal/Fragments/FragmentRecipeAdd.kt
@@ -0,0 +1,23 @@
+package com.example.FoodIepal.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.FoodIepal.R
+
+class FragmentRecipeAdd : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_recipe_add, container, false)
+    }
+
+    companion object {
+
+        fun newInstance() = FragmentRecipeAdd()
+    }
+}
---
+++ app/src/main/java/com/example/FoodIepal/Utils/FatSecterApi.kt
@@ -0,0 +1,6 @@
+package com.example.FoodIepal.Utils
+
+class FatSecterApi {
+    val ApiKey = "38c2bde7b5184df0ae6e8ce71c6cfd96"
+
+}
