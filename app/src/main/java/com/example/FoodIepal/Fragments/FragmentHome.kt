627699e9be58a631ef7db446f4fc699390450309
TeaOk210 <danilplay21006@gmail.com>
2023-04-10 17:41:22 +0200

    напиши гет фуд и разберись со своей асинхронностью

--- app/src/main/java/com/example/FoodIepal/Fragments/FragmentHome.kt
+++ app/src/main/java/com/example/FoodIepal/Fragments/FragmentHome.kt
@@ -2,6 +2,7 @@ package com.example.FoodIepal.Fragments
 
 import android.annotation.SuppressLint
 import android.content.ClipData.Item
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,13 +14,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.FoodIepal.R
 import com.example.FoodIepal.RecipeAdapter
 import com.example.FoodIepal.RecipeItem
+import com.example.FoodIepal.Utils.FatSecretGet
 import com.example.FoodIepal.databinding.FragmentHomeMenuBinding
+import org.json.JSONObject
 
 
+@Suppress("DEPRECATION")
 class FragmentHome : Fragment() {
     var RecipeItemList = ArrayList<RecipeItem>()
     lateinit var adapter: RecipeAdapter
     lateinit var binding : FragmentHomeMenuBinding
+    lateinit var mFatSecretGet: FatSecretGet
     var ItemCount: Int = 0
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
@@ -45,6 +50,17 @@ class FragmentHome : Fragment() {
         val recipeItem = RecipeItem(name = name, text = text, time = time, Kkal = Kkal, RecipeImageResId = R.drawable.food)
         RecipeItemList.add(recipeItem)
         }
+
+    private fun getFood(id: Long) {
+        AsyncTask<String, String, String>().execute() {
+            val foodGet: JSONObject? = mFatSecretGet.getFood(id)
+            try {
+                if(foodGet != null) {
+                    val food_name: String = foodGet.
+                }
+            }
+        }
+    }
 
     fun SetUpAdapter(){
         adapter = RecipeAdapter(requireActivity(), RecipeItemList )
